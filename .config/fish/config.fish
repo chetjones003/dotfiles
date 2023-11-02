f2d343a27b9d9243dee019dcd57c4cc137a5c0ed
Chet Jones <chetjones003@gmail.com>
2023-11-02 15:35:17 -0500

    feat: new fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,36 +1,72 @@
-### ADDING TO THE PATH
-# First line removes the path; second line sets it.  Without the first line,
-# your path gets massive and fish becomes very slow.
 set -e fish_user_paths
-set -U fish_user_paths $HOME/.bin  $HOME/.local/bin $HOME/.config/emacs/bin $HOME/Applications /var/lib/flatpak/exports/bin/ $fish_user_paths /usr/local/go/bin
+set -U fish_user_paths $HOME/.config/emacs/bin/ $HOME/.bin $HOME/.cargo/bin $HOME/.local/bin $HOME/.config/emacs/bin $HOME/Applications /var/lib/flatpak/exports/bin/ $fish_user_paths
 
 ### EXPORT ###
 set fish_greeting                                 # Supresses fish's intro message
 set TERM "xterm-256color"                         # Sets the terminal type
-set EDITOR "emacsclient -t -a ''"                 # $EDITOR use Emacs in terminal
-set VISUAL "emacsclient -c -a emacs"              # $VISUAL use Emacs in GUI mode
+set EDITOR "nvim"                 # $EDITOR use Emacs in terminal
+set VISUAL "nvim"              # $VISUAL use Emacs in GUI mode
 
 ### SET MANPAGER
+### Uncomment only one of these!
+
+### "bat" as manpager
+# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+### "vim" as manpager
+# set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+
 ### "nvim" as manpager
 set -x MANPAGER "nvim -c 'set ft=man' -"
 
+### "less" as manpager
+#set -x MANPAGER "less"
+
 ### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
 function fish_user_key_bindings
-    bind -M insert \cy accept-autosuggestion
-
-    fish_vi_key_bindings
+  # fish_default_key_bindings
+  fish_vi_key_bindings
 end
 ### END OF VI MODE ###
 
-# vim and emacs
-alias vim='nvim'
+bind -M insert \cy accept-autosuggestion
 
-alias nvimconf='cd $HOME/.config/nvim && nvim'
-alias fishconf='cd $HOME/dotfiles/.config/fish/ && nvim'
-alias alacrittyconf='cd $HOME/dotfiles/.config/alacritty/ && nvim'
-alias starshipconf='cd $HOME/dotfiles/.config/starship.toml'
-alias dotfiles='cd $HOME/dotfiles'
+# TokyoNight Color Palette
+set -l foreground c0caf5
+set -l selection 2e3c64
+set -l comment 565f89
+set -l red f7768e
+set -l orange ff9e64
+set -l yellow e0af68
+set -l green 9ece6a
+set -l purple 9d7cd8
+set -l cyan 7dcfff
+set -l pink bb9af7
 
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
+set -g fish_pager_color_selected_background --background=$selection
+
+### ALIASES ###
 # Changing "ls" to "exa"
 alias ls='exa -al --color=always --group-directories-first' # my preferred listing
 alias la='exa -a --color=always --group-directories-first'  # all files and dirs
@@ -38,13 +74,67 @@ alias ll='exa -l --color=always --group-directories-first'  # long format
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
 alias l.='exa -a | egrep "^\."'
 
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
 # confirm before overwriting something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
 
-# helper for nmcli
-alias wificonnect="echo \n'nmcli device wifi list\nnmcli device rescan\nnmcli device connect <ssid> password <password>\nnmcli connection show'"
+# ps
+alias psa="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+alias psmem='ps auxf | sort -nr -k 4'
+alias pscpu='ps auxf | sort -nr -k 3'
 
-### SETTING THE STARSHIP PROMPT ###
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias tag='git tag'
+alias newtag='git tag -a'
+
+# switch between shells
+# I do not recommend switching default SHELL from bash.
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+alias cb="cargo build"
+alias cr="cargo run"
+alias cc="cargo check"
+alias ca="cargo add"
+alias cn="cargo new"
+
+alias work="cd /mnt/c/Users/chetj/Documents/Desktop"
+
+
+# setup node and typescript with testing
+function node-ts-test
+    npm init -y
+    npm i -D typescript ts-node jest ts-jest @types/node @types/jest
+    tsc --init --sourceMap --rootDir src --outDir dist --target ES2020 --moduleResolution node
+    npx ts-jest config:init
+    npm init @eslint/config
+end
+alias npmProjGenWithTest=node-ts-test
+
 starship init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# opam configuration
+source /home/chetj/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/chetj/.ghcup/bin # ghcup-env
